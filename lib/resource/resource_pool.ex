28af542480bd5d079f4d847fea0edcedc9138c5d
Fabian Muscariello <nroi@mailbox.org>
2019-05-04 11:55:15 +0200

    Update

--- lib/resource/resource_pool.ex
+++ lib/resource/resource_pool.ex
@@ -2,12 +2,13 @@ defmodule Resource.ResourcePool do
   use GenServer
   require Logger
 
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+  def start_link(initial_state) do
+    GenServer.start_link(__MODULE__, initial_state, [])
   end
 
   @impl true
-  def init(:ok) do
+  def init(initial_state) do
+    Logger.debug("Starting gen_server with #{inspect initial_state}")
     # default implementation to avoid warning.
     {:ok, %{}}
   end
--- test/resource_test.exs
+++ test/resource_test.exs
@@ -3,7 +3,7 @@ defmodule ResourceTest do
   doctest Resource
 
   setup do
-    resource_pool = start_supervised!(Resource.ResourcePool)
+    resource_pool = start_supervised!({Resource.ResourcePool, :ok_foo})
     %{resource_pool: resource_pool}
   end
 
