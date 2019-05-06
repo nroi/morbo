3afe8fe6d5013db647f2a8f243df1b454ca6c1c4
Fabian Muscariello <nroi@mailbox.org>
2019-05-06 17:29:18 +0200

    Update

--- test/hackney_resource_test.exs
+++ test/hackney_resource_test.exs
@@ -6,7 +6,7 @@ defmodule HackneyResourceTest do
   @remove_resource_after 250
 
   setup do
-    init_state = get_init_state
+    init_state = get_init_state()
     resource_pool = start_supervised!({Resource.ResourcePool, init_state})
     %{resource_pool: resource_pool}
   end
@@ -17,18 +17,27 @@ defmodule HackneyResourceTest do
                         {:ok, conn_ref} = :hackney.connect(:hackney_ssl, hostname, 443, [])
                         conn_ref
       end,
-      transfer_ownership_to: fn _new_pid, _conn_ref -> :ok end,
+      transfer_ownership_to: fn new_pid, conn_ref ->
+        :hackney.controlling_process(conn_ref, new_pid)
+      end,
       close_spawn: fn conn_ref -> :ok = :hackney.close(conn_ref) end,
       resources: [],
       remove_resource_after: @remove_resource_after
     }
   end
 
-  @tag :wip
   test "connection fetched from existing connection", %{resource_pool: _resource_pool} do
     {:new_spawn, conn_ref} = Resource.ResourcePool.resource_request(@hostname)
     :ok = Resource.ResourcePool.release_resource(@hostname)
     {:existing_spawn, ^conn_ref} = Resource.ResourcePool.resource_request(@hostname)
+    :ok = Resource.ResourcePool.release_resource(@hostname)
+  end
+
+  @tag :wip
+  test "Execute some GET requests", %{resource_pool: _resource_pool} do
+    {:new_spawn, conn_ref} = Resource.ResourcePool.resource_request(@hostname)
+    req = {:get, "/", [], ""}
+    {:ok, _, _, ^conn_ref} = :hackney.send_request(conn_ref, req)
   end
 
 end
